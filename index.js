0b95c90641f3e8933d01c5be3d4ee831ce1a5b78
faizanUnico <faizanzaid789@gmail.com>
2023-03-19 12:25:23 +0530

    last commit

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-import {config} from "dotenv"
-config();
-import { Configuration, OpenAIApi} from "openai"
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+const express=require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const ejs=require('ejs');
 
-const configuration = new Configuration({
-  apiKey: process.env.API_KEY,
-});
-// configuring our api key in OpenAIApi configuration...
-const openai = new OpenAIApi(configuration);
+const app=express();
+app.set("view engine", "ejs");
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('public', express.static('public', ))
 
-openai.createChatCompletion({
-	model:"gpt-3.5-turbo",
-	messages:[{role:"user", content:"Hello, how are you doing?"}]
-}).then(res=>{
-	console.log(res.data.choices[0])
+app.get("/", (req, res)=>{
+	res.render("index", {data: ''})
+})
+
+app.post("/ask-gpt", (req, res)=>{
+	const configuration = new Configuration({
+		apiKey: process.env.API_KEY,
+	});
+	const openAI = new OpenAIApi(configuration);
+	
+
+	if(req.body.message===''){
+		return res.render("index", {data: 'Please enter a message'});
+	}
+	
+
+
+	openAI.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages:[{role: "user", content: req.body.message}],
+	}).then((data)=>{
+		
+
+		res.render("index", {data: data.data.choices[0].message.content});
+	}).catch((err)=>{
+		console.log(err);
+		res.render("index", {data: 'Error'});
+	})
+
+
+})
+
+app.listen(3000, ()=>{
+console.log("Listening on this port")
 })
 
